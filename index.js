75346844d613dfa5865316c50e481050ebedeeb1
Federico Rampazzo <frampone@gmail.com>
2014-12-02 23:43:45 +0000

    fix empty properties

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ module.exports = function(Handlebars, path){
   if (!path)
     path = '__language';
   Handlebars.registerHelper('tr', function(context, options){
-    if (!options.data.root[path][context])
-      return options.fn(this);
+    if (!options.data.root[path] || !options.data.root[path][context])
+      return '';
     var template = Handlebars.compile(options.data.root[path][context]);
     return template(options.data.root);  
   });
